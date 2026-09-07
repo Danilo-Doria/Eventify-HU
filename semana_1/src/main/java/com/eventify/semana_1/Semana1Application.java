98c94d1414ca6085e8ef6150dc5d55ae3e660ed0
Danilo Andres Doria Diaz <danilodoria519@gmail.com>
2026-09-07 17:24:15 -0500

    Initial structure

---
+++ semana_1/src/main/java/com/eventify/semana_1/Semana1Application.java
@@ -0,0 +1,13 @@
+package com.eventify.semana_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semana1Application.class, args);
+    }
+
+}
