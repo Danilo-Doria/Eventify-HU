6fbd10b8ad9a9b23a9f92001618c7a7fcd205802
Danilo Andres Doria Diaz <danilodoria519@gmail.com>
2026-09-13 09:56:16 -0500

    update models avoiding @Data

--- semana_1/src/main/java/com/eventify/semana_1/model/Venue.java
+++ semana_1/src/main/java/com/eventify/semana_1/model/Venue.java
@@ -1,10 +1,9 @@
 package com.eventify.semana_1.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Venue {
